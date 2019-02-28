3e49d7603835bf7751e037e66e326b7c8bf87624
DMT <dimapuz97@gmail.com>
2019-02-28 15:07:56 +0200

    log

--- src/pow.cpp
+++ src/pow.cpp
@@ -120,12 +120,14 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > Params().ProofOfWorkLimit())
-        return error("CheckProofOfWork() : nBits below minimum work");
+        return error("CheckProofOfWork() : nBits below minimum work ");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget)
+    if (hash > bnTarget){
+        LogPrintf("hash = ", hash);
+        LogPrintf("bits = ",bnTarget);
         return error("CheckProofOfWork() : hash doesn't match nBits");
-
+    }
     return true;
 }
 
