226c038c79701392769570c703c3af2035108eed
DMT <dimapuz97@gmail.com>
2019-02-28 15:14:21 +0200

    log

--- src/pow.cpp
+++ src/pow.cpp
@@ -124,8 +124,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget){
-        LogPrintf("hash = ", hash);
-        LogPrintf("bits = ",bnTarget);
+        LogPrintf("hash = %s", hash);
+        LogPrintf("bits = %s",bnTarget);
         return error("CheckProofOfWork() : hash doesn't match nBits");
     }
     return true;
